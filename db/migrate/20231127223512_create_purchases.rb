edf5fd5408ba481a2ffbd38d686fe10e61ad61e5
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-11-28 00:02:33 +0100

    Fix migration files, and add name field for registration, and profile edit

--- db/migrate/20231127223512_create_purchases.rb
+++ db/migrate/20231127223512_create_purchases.rb
@@ -1,6 +1,9 @@
 class CreatePurchases < ActiveRecord::Migration[7.1]
   def change
     create_table :purchases do |t|
+      t.integer :author_id
+      t.string :name
+      t.decimal :amount
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_27_211559) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_27_223512) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "groups", force: :cascade do |t|
-    t.string "name"
-    t.string "icon"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "purchases", force: :cascade do |t|
-    t.integer "author_id"
-    t.string "name"
-    t.decimal "amount"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
