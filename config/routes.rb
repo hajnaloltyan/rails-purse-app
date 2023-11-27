8bd6c44790d214c84f625492404c5ce417c7817d
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-11-27 22:33:16 +0100

    Fix linters

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   authenticated :user do
-    root to: "home#index", as: :authenticated_root
+    root to: "groups#index", as: :authenticated_root
   end
 
   root to: "devise/sessions#new"
