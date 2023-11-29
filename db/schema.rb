26a843775fd5650d65da37c93e247cdeba0fd538
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-11-29 22:23:57 +0100

    Update models

--- db/schema.rb
+++ db/schema.rb
@@ -15,9 +15,20 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_27_223512) do
   enable_extension "plpgsql"
 
   create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
   create_table "purchases", force: :cascade do |t|
+    t.integer "author_id"
+    t.string "name"
+    t.decimal "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,4 +44,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_27_223512) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "groups", "users"
 end
