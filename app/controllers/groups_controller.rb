9f6ca72f3de22f871052214ae7c7852edc26ae63
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-11-29 22:24:13 +0100

    Update controllers

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,69 +1,49 @@
 class GroupsController < ApplicationController
   before_action :set_group, only: %i[show edit update destroy]
 
-  # GET /groups or /groups.json
   def index
-    @groups = Group.all
+    @groups = Group.all.includes(:user)
   end
 
-  # GET /groups/1 or /groups/1.json
   def show; end
 
-  # GET /groups/new
   def new
     @group = Group.new
   end
 
-  # GET /groups/1/edit
   def edit; end
 
-  # POST /groups or /groups.json
   def create
-    @group = Group.new(group_params)
+    @group = current_user.groups.new(group_params)
 
-    respond_to do |format|
-      if @group.save
-        format.html { redirect_to group_url(@group), notice: 'Group was successfully created.' }
-        format.json { render :show, status: :created, location: @group }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @group.errors, status: :unprocessable_entity }
-      end
+    if @group.save
+      redirect_to group_url(@group), notice: 'Group was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /groups/1 or /groups/1.json
   def update
-    respond_to do |format|
-      if @group.update(group_params)
-        format.html { redirect_to group_url(@group), notice: 'Group was successfully updated.' }
-        format.json { render :show, status: :ok, location: @group }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @group.errors, status: :unprocessable_entity }
-      end
+    if @group.update(group_params)
+      redirect_to group_url(@group), notice: 'Group was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /groups/1 or /groups/1.json
   def destroy
     @group.destroy!
 
-    respond_to do |format|
-      format.html { redirect_to groups_url, notice: 'Group was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to groups_url, notice: 'Group was successfully deleted.'
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_group
     @group = Group.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def group_params
-    params.fetch(:group, {})
+    params.require(:group).permit(:name, :icon)
   end
 end
--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,68 +1,48 @@
 class PurchasesController < ApplicationController
   before_action :set_purchase, only: %i[show edit update destroy]
 
-  # GET /purchases or /purchases.json
   def index
     @purchases = Purchase.all
   end
 
-  # GET /purchases/1 or /purchases/1.json
   def show; end
 
-  # GET /purchases/new
   def new
     @purchase = Purchase.new
   end
 
-  # GET /purchases/1/edit
   def edit; end
 
-  # POST /purchases or /purchases.json
   def create
     @purchase = Purchase.new(purchase_params)
 
-    respond_to do |format|
-      if @purchase.save
-        format.html { redirect_to purchase_url(@purchase), notice: 'Purchase was successfully created.' }
-        format.json { render :show, status: :created, location: @purchase }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @purchase.errors, status: :unprocessable_entity }
-      end
+    if @purchase.save
+      redirect_to purchase_url(@purchase), notice: 'Purchase was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /purchases/1 or /purchases/1.json
   def update
-    respond_to do |format|
-      if @purchase.update(purchase_params)
-        format.html { redirect_to purchase_url(@purchase), notice: 'Purchase was successfully updated.' }
-        format.json { render :show, status: :ok, location: @purchase }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @purchase.errors, status: :unprocessable_entity }
-      end
+    if @purchase.update(purchase_params)
+      redirect_to purchase_url(@purchase), notice: 'Purchase was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /purchases/1 or /purchases/1.json
   def destroy
     @purchase.destroy!
 
-    respond_to do |format|
-      format.html { redirect_to purchases_url, notice: 'Purchase was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to purchases_url, notice: 'Purchase was successfully deleted.'
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_purchase
-    @purchase = Purchase.find(params[:id])
+    @purchase = Purchase.include(:users).find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def purchase_params
     params.fetch(:purchase, {})
   end
