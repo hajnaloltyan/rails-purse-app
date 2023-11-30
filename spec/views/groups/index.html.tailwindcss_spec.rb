83c64f594d135e860d8a32cc3359e9169835f11e
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-11-30 22:42:32 +0100

    Write tests for the groups index page view

--- spec/views/groups/index.html.tailwindcss_spec.rb
+++ spec/views/groups/index.html.tailwindcss_spec.rb
@@ -1,15 +1,32 @@
 require 'rails_helper'
+require 'capybara/rails'
 
-RSpec.describe 'groups/index', type: :view do
-  before(:each) do
-    assign(:groups, [
-             Group.create!,
-             Group.create!
-           ])
+RSpec.describe 'Groups Index Page', type: :system do
+  fixtures :users, :groups
+
+  before do
+    user = users(:one)
+    sign_in user
   end
 
-  it 'renders a list of groups' do
-    render
-    Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
+  it 'displays the group name' do
+    visit groups_path
+    expect(page).to have_content('Tools')
+  end
+
+  it 'displays the log out' do
+    visit groups_path
+    expect(page).to have_content('Log out')
+  end
+
+  it 'displays the button to create a new group' do
+    visit groups_path
+    expect(page).to have_content('New group')
+  end
+
+  it 'leads the user to the purchases of the group page' do
+    visit groups_path
+    click_on 'Tools'
+    expect(page).to have_content('Tools')
   end
 end
--- spec/views/groups/show.html.tailwindcss_spec.rb
+++ spec/views/groups/show.html.tailwindcss_spec.rb
@@ -1,11 +1,43 @@
 require 'rails_helper'
+require 'capybara/rails'
 
-RSpec.describe 'groups/show', type: :view do
-  before(:each) do
-    assign(:group, Group.create!)
+RSpec.describe 'Groups Show Page', type: :system do
+  fixtures :users, :groups, :purchases
+
+  before do
+    user = users(:one)
+    sign_in user
+    @group = groups(:one)
   end
 
-  it 'renders attributes in <p>' do
-    render
+  it 'displays the group name' do
+    visit group_path(@group)
+    expect(page).to have_content('Tools')
+  end
+
+  it 'displays the purchases of the group' do
+    visit group_path(@group)
+    expect(page).to have_content('Hammer')
+  end
+
+  it 'displays the purchase amount' do
+    visit group_path(@group)
+    expect(page).to have_content(12.00)
+  end
+
+  it 'diplays the new purchase button' do
+    visit group_path(@group)
+    expect(page).to have_content('New Purchase')
+  end
+
+  it 'leads the user to the details of the purchase page' do
+    visit group_path(@group)
+    click_on 'Hammer'
+    expect(page).to have_content('Hammer')
+  end
+
+  it 'displays the delete button' do
+    visit group_path(@group)
+    expect(page).to have_content('Delete this Group')
   end
 end
