8afd7b265eacb5c39dddd8ee2575064529ff9793
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-12-01 00:10:12 +0100

    Remove flowbite completely

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,23 +1,23 @@
-import { fontFamily as _fontFamily } from '@tailwindcss/defaultTheme'
+const defaultTheme = require('tailwindcss/defaultTheme')
 
-export const content = [
-  './node_modules/flowbite/**/*.js',
-  './public/*.html',
-  './app/helpers/**/*.rb',
-  './app/javascript/**/*.js',
-  './app/views/**/*.{erb,haml,html,slim}'
-]
-export const theme = {
-  extend: {
-    fontFamily: {
-      sans: ['Inter var', ..._fontFamily.sans],
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
     },
   },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+  ]
 }
-export const plugins = [
-  require('@tailwindcss/forms'),
-  require('@tailwindcss/aspect-ratio'),
-  require('@tailwindcss/typography'),
-  require('@tailwindcss/container-queries'),
-  require('@flowbite/plugin'),
-]
