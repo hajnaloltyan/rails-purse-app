f67eb40fb067725c94e501f64c07e1f9038b21c2
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-12-01 00:02:14 +0100

    Flowbite causing deployment issue - fix 4

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,25 +1,23 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+import { fontFamily as _fontFamily } from '@tailwindcss/defaultTheme'
 
-module.exports = {
-  content: [
-    './node_modules/flowbite/**/*.js',
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
+export const content = [
+  './node_modules/flowbite/**/*.js',
+  './public/*.html',
+  './app/helpers/**/*.rb',
+  './app/javascript/**/*.js',
+  './app/views/**/*.{erb,haml,html,slim}'
+]
+export const theme = {
+  extend: {
+    fontFamily: {
+      sans: ['Inter var', ..._fontFamily.sans],
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-    require('@flowbite/plugin'),
-  ]
 }
+export const plugins = [
+  require('@tailwindcss/forms'),
+  require('@tailwindcss/aspect-ratio'),
+  require('@tailwindcss/typography'),
+  require('@tailwindcss/container-queries'),
+  require('@flowbite/plugin'),
+]
