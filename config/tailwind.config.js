e91b7d0b6dbda97c93f2a46cc13a0c845c88c45d
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-11-30 23:19:37 +0100

    Fix deployment

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -14,6 +14,7 @@ module.exports = {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
     },
+    defaultTheme: 'default',
   },
   plugins: [
     require('flowbite/plugin'),
