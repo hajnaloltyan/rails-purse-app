2ddc44dcb247507e1744fd5a81c11019a9925328
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-12-01 18:11:53 +0100

    Add purchase  model tests

--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Purchase, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :purchases
+
+  it 'has a valid purchase' do
+    expect(purchases(:one)).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    purchase = purchases(:one)
+    purchase.name = nil
+    expect(purchase).not_to be_valid
+  end
+
+  it 'is invalid without a group' do
+    purchase = purchases(:one)
+    purchase.group = nil
+    expect(purchase).not_to be_valid
+  end
+
+  it 'is invalid without an amount' do
+    purchase = purchases(:one)
+    purchase.amount = nil
+    expect(purchase).not_to be_valid
+  end
 end
