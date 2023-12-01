54c7af2c37b2ae6a6fdd3313d1fe72f6f7b3603b
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-12-01 18:00:19 +0100

    Add user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :users
+
+  it 'has a valid factory' do
+    expect(users(:one)).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    user = users(:one)
+    user.name = nil
+    expect(user).not_to be_valid
+  end
+
+  it 'is invalid without an email' do
+    user = users(:one)
+    user.email = nil
+    expect(user).not_to be_valid
+  end
 end
